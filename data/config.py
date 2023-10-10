668838ede360645c361d7d3e1275d3bbe853a5db
k3menn <101514543+k3menn@users.noreply.github.com>
2023-10-10 13:28:13 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,4 +1,4 @@
 GATEIO_KEY = {
-    'api_key': 'f2d82c2707a253309d15878feba57b74',
-    'api_secret': '31c5e6126ed4537bf02dc324a4d7b3bb5c5b2efea1e4398910d11d4cdb7513b1',
+    'api_key': '',
+    'api_secret': '',
 }
